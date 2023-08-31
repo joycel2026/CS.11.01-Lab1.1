cba93a90f0af6e5473c9d388c9e54dff681b7f49
joyceli <joycel2026@student.cis.edu.hk>
2023-08-31 18:57:38 +0800

    Added main method and print statement.

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner1 = new Scanner (System.in);
+        System.out.println("PLease enter your age:");
+        int age = scanner1.nextInt();
+        System.out.println(age);
+        int cal1 = age/2;
+        int cal2 = cal1+7;
+        String result = age+"-years old should date somebody who is at least "+cal2+"years old.";
+        System.out.println(result);
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner1 = new Scanner (System.in);
+        System.out.println("PLease enter your age:");
+        int Age = scanner1.nextInt();
+        System.out.println(Age);
+        int result = 100 - Age;
+        String Output = Age+"-years old should read at least "+result+" pages before giving up on a book.";
+        System.out.println(Output);
     }
 }
